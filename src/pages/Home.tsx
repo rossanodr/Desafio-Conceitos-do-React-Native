f854e341d0f3a65422627a6b5b44785b98bde91e
rossanodr <rvdalarosa@hotmail.com>
2022-08-03 15:10:12 -0300

    Tarefa 2

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,28 @@
 import React, { useState } from "react";
-import { StyleSheet, View } from "react-native";
+import { Alert, StyleSheet, View } from "react-native";
 
 import { Header } from "../components/Header";
 import { Task, TasksList } from "../components/TasksList";
 import { TodoInput } from "../components/TodoInput";
 
+export type EditTaskArgs ={ 
+taskId: Number;
+taskNewTitle: string;
+}
 export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
+
   function handleAddTask(newTaskTitle: string) {
+    // Checking if the task has already been added
+    const taskWithSameTitle = tasks.find((task) => task.title === newTaskTitle);
+    if (taskWithSameTitle) {
+      return Alert.alert(
+        "Task já cadastrada",
+        "Você não pode cadastrar uma task com o mesmo nome"
+      );
+    }
+
     // TODO - add new task
     const newTask = {
       id: new Date().getTime(),
@@ -20,22 +34,48 @@ export function Home() {
 
   function handleToggleTaskDone(id: number) {
     //TODO - toggle task done if exists
-    const updatedTasksList = tasks.map(task => ({...task}))
+    const updatedTasksList = tasks.map((task) => ({ ...task }));
 
-    const toggleTask = updatedTasksList.find(i => i.id === id);
-    if (!toggleTask){
-      return
-      } else {
-        toggleTask.done = !toggleTask.done;
-        setTasks(updatedTasksList)
-      }
-
+    const toggleTask = updatedTasksList.find((i) => i.id === id);
+    if (!toggleTask) {
+      return;
+    } else {
+      toggleTask.done = !toggleTask.done;
+      setTasks(updatedTasksList);
+    }
   }
 
   function handleRemoveTask(id: number) {
-    //TODO - remove task from state
-    const updatedTasksList = tasks.filter((task) => task.id !== id);
-    setTasks(updatedTasksList);
+    Alert.alert(
+      "Remover item",
+      "Tem certeza que você deseja remover este item?",
+      [
+        {
+          style: "cancel",
+          text: "Não",
+        },
+        {
+          style: "destructive",
+          text: "Sim",
+          onPress: () => {
+            //TODO - remove task from state
+            const updatedTasksList = tasks.filter((task) => task.id !== id);
+            setTasks(updatedTasksList);
+          },
+        },
+      ]
+    );
+  }
+
+  function handleEditTask({taskId, taskNewTitle}: EditTaskArgs) {
+    const updatedTasksList = tasks.map((task) => ({ ...task }));
+    const taskToBeEdited = updatedTasksList.find((i) => i.id === taskId);
+    if (!taskToBeEdited) {
+      return;
+    } else {
+      taskToBeEdited.title = taskNewTitle;
+      setTasks(updatedTasksList);
+    }
   }
 
   return (
@@ -48,6 +88,7 @@ export function Home() {
         tasks={tasks}
         toggleTaskDone={handleToggleTaskDone}
         removeTask={handleRemoveTask}
+        editTask={handleEditTask}
       />
     </View>
   );
